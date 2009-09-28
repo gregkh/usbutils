93aa80bfbd35d0ff27704a173ecba5d8463ff183
Greg Kroah-Hartman <gregkh@suse.de>
2009-09-28 10:27:29 -0700

    coding style cleanups for .h files.
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- list.h
+++ list.h
@@ -30,9 +30,9 @@ struct list_head {
  * This is only for internal list manipulation where we know
  * the prev/next entries already!
  */
-static __inline__ void __list_add(struct list_head *new,
-				  struct list_head *prev,
-				  struct list_head *next)
+static inline void __list_add(struct list_head *new,
+			      struct list_head *prev,
+			      struct list_head *next)
 {
 	next->prev = new;
 	new->next = next;
@@ -43,7 +43,7 @@ static __inline__ void __list_add(struct list_head *new,
 /*
  * Insert a new entry after the specified head..
  */
-static __inline__ void list_add(struct list_head *new, struct list_head *head)
+static inline void list_add(struct list_head *new, struct list_head *head)
 {
 	__list_add(new, head, head->next);
 }
@@ -51,8 +51,7 @@ static __inline__ void list_add(struct list_head *new, struct list_head *head)
 /*
  * Insert a new entry before the specified head..
  */
-static __inline__ void list_add_tail(struct list_head *new,
-				     struct list_head *head)
+static inline void list_add_tail(struct list_head *new, struct list_head *head)
 {
 	__list_add(new, head->prev, head);
 }
@@ -64,19 +63,18 @@ static __inline__ void list_add_tail(struct list_head *new,
  * This is only for internal list manipulation where we know
  * the prev/next entries already!
  */
-static __inline__ void __list_del(struct list_head *prev,
-				  struct list_head *next)
+static inline void __list_del(struct list_head *prev, struct list_head *next)
 {
 	next->prev = prev;
 	prev->next = next;
 }
 
-static __inline__ void list_del(struct list_head *entry)
+static inline void list_del(struct list_head *entry)
 {
 	__list_del(entry->prev, entry->next);
 }
 
-static __inline__ int list_empty(struct list_head *head)
+static inline int list_empty(struct list_head *head)
 {
 	return head->next == head;
 }
@@ -84,7 +82,7 @@ static __inline__ int list_empty(struct list_head *head)
 /*
  * Splice in "list" into "head"
  */
-static __inline__ void list_splice(struct list_head *list,
+static inline void list_splice(struct list_head *list,
 				   struct list_head *head)
 {
 	struct list_head *first = list->next;
