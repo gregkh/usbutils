34a9d61e30024677b2c761c38fd93b249a8a52bd
Nikolai Kondrashov <spbnick@gmail.com>
2010-06-28 19:48:48 +0000

    First (non-working) draft compiles.

--- usbhid-dump/src/hid-dump.c
+++ usbhid-dump/src/hid-dump.c
@@ -24,6 +24,16 @@
  * @(#) $Id$
  */
 
+#include <assert.h>
+#include <stdbool.h>
+#include <ctype.h>
+#include <string.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <getopt.h>
+#include <stdio.h>
+#include <libusb-1.0/libusb.h>
 
 static bool
 usage(FILE *stream, const char *progname)
@@ -35,16 +45,253 @@ usage(FILE *stream, const char *progname)
             "Dump a USB device HID report descriptor or stream."
             "\n"
             "Arguments:\n"
-            "  entity   \"descriptor\" or \"stream\", can be abbreviated\n"
+            "  entity   \"descriptor\", \"stream\" or \"both\";\n"
+            "           can be abbreviated\n"
             "  bus      bus number\n"
             "  dev      device number\n"
-            "  if       interface number; can be ommitted,\n"
-            "           if the device has only one HID interface\n"
+            "  if       interface number; if ommitted,\n"
+            "           all the device HID interfaces are dumped\n"
             "\n"
             "Options:\n"
-            "  -h, --help   this help message\n",
-            "\n"
+            "  -h, --help   this help message\n"
+            "\n",
             progname) >= 0;
+}
+
+
+static const char *
+libusb_strerror(enum libusb_error err)
+{
+    switch (err)
+    {
+        case LIBUSB_SUCCESS:
+            return "Success";
+#define MAP(_name, _desc) \
+    case LIBUSB_ERROR_##_name:          \
+        return _desc " (" #_name ")"
+	    MAP(IO,
+            "Input/output error");
+	    MAP(INVALID_PARAM,
+            "Invalid parameter");
+	    MAP(ACCESS,
+            "Access denied (insufficient permissions)");
+	    MAP(NO_DEVICE,
+            "No such device (it may have been disconnected)");
+	    MAP(NOT_FOUND,
+            "Entity not found");
+	    MAP(BUSY,
+            "Resource busy");
+	    MAP(TIMEOUT,
+            "Operation timed out");
+	    MAP(OVERFLOW,
+            "Overflow");
+	    MAP(PIPE,
+            "Pipe error");
+	    MAP(INTERRUPTED,
+            "System call interrupted (perhaps due to signal)");
+	    MAP(NO_MEM,
+            "Insufficient memory");
+	    MAP(NOT_SUPPORTED,
+            "Operation not supported or unimplemented on this platform");
+	    MAP(OTHER, "Other error");
+#undef MAP
+        default:
+            return "Unknown error code";
+    }
+}
+
+
+static bool
+strisblank(const char *str)
+{
+    for (; *str != '\0'; str++)
+        if (!isblank(*str))
+            return false;
+    return true;
+}
+
+#define ERROR_CLEANUP(_fmt, _args...) \
+    do {                                                    \
+        fprintf(stderr, "Failed to " _fmt "\n", ##_args);   \
+        goto cleanup;                                       \
+    } while (0)
+
+#define LIBUSB_ERROR_CLEANUP(_fmt, _args...) \
+    do {                                        \
+        fprintf(stderr,                         \
+                "Failed to " _fmt ": %s\n",     \
+                ##_args, libusb_strerror(err)); \
+        goto cleanup;                           \
+    } while (0)
+
+
+enum libusb_error
+libusb_open_device_with_bus_dev(libusb_context         *ctx,
+                                uint8_t                 bus_num,
+                                uint8_t                 dev_addr,
+                                libusb_device_handle  **phandle)
+{
+    enum libusb_error       err     = LIBUSB_ERROR_OTHER;
+    libusb_device         **list    = NULL;
+    ssize_t                 num;
+    ssize_t                 idx;
+    libusb_device          *dev;
+    libusb_device_handle   *handle  = NULL;
+
+    /* Retrieve device list */
+    num = libusb_get_device_list(ctx, &list);
+    if (num == LIBUSB_ERROR_NO_MEM)
+    {
+        err = num;
+        goto cleanup;
+    }
+
+    /* Find the device */
+    for (idx = 0; idx < num; idx++)
+    {
+        dev = list[idx];
+
+        if (libusb_get_bus_number(dev) == bus_num &&
+            libusb_get_device_address(dev) == dev_addr)
+        {
+            err = libusb_open(dev, &handle);
+            if (err != LIBUSB_SUCCESS)
+                goto cleanup;
+            break;
+        }
+    }
+
+    /* Free the device list freeing unused devices */
+    libusb_free_device_list(list, true);
+    list = NULL;
+
+    /* Check if the device is found */
+    if (handle == NULL)
+    {
+        err = LIBUSB_ERROR_NO_DEVICE;
+        goto cleanup;
+    }
+
+    /* Output the handle */
+    if (phandle != NULL)
+    {
+        *phandle = handle;
+        handle = NULL;
+    }
+
+    err = LIBUSB_SUCCESS;
+
+cleanup:
+
+    /* Free the device */
+    if (handle != NULL)
+        libusb_close(handle);
+
+    /* Free device list along with devices */
+    if (list != NULL)
+        libusb_free_device_list(list, true);
+
+    return err;
+}
+
+
+ssize_t
+libusb_find_interfaces_by_class(libusb_device          *dev,
+                                uint8_t                 if_class,
+                                int                   **pif_list,
+                                size_t                 *pif_num)
+{
+    enum libusb_error                   err     = LIBUSB_ERROR_OTHER;
+    struct libusb_config_descriptor    *config  = NULL;
+    const struct libusb_interface      *iface;
+    uint8_t                             i;
+
+    (void)if_class;
+    (void)pif_list;
+    (void)pif_num;
+
+    err = libusb_get_active_config_descriptor(dev, &config);
+    if (err != LIBUSB_SUCCESS)
+        goto cleanup;
+
+    for (iface = config->interface, i = 0;
+         i < config->bNumInterfaces;
+         i++, iface++)
+    {
+        fprintf(stderr, "if index: %hhu, alt settings: %d\n",
+                i, iface->num_altsetting);
+    }
+    
+
+cleanup:
+
+    if (config != NULL)
+        libusb_free_config_descriptor(config);
+
+    return err;
+}
+
+#if 0
+static int
+run_handle(bool                     dump_descriptor,
+           bool                     dump_stream,
+           libusb_device_handle    *handle,
+           int                      if_num)
+{
+}
+#endif
+
+
+static int
+run(bool    dump_descriptor,
+    bool    dump_stream, 
+    uint8_t bus_num,
+    uint8_t dev_addr,
+    int     if_num)
+{
+    int                     result  = 1;
+    enum libusb_error       err;
+    libusb_context         *ctx     = NULL;
+    libusb_device         **list    = NULL;
+    libusb_device_handle   *handle  = NULL;
+
+    /* Initialize libusb context */
+    err = libusb_init(&ctx);
+    if (err != LIBUSB_SUCCESS)
+        LIBUSB_ERROR_CLEANUP("create libusb context");
+
+    /* Set libusb debug level */
+    libusb_set_debug(ctx, 3);
+
+    /* Find and open the device */
+    err = libusb_open_device_with_bus_dev(ctx, bus_num, dev_addr, &handle);
+    if (err != LIBUSB_SUCCESS)
+        LIBUSB_ERROR_CLEANUP("find and open the device");
+
+#if 0
+    /* Run with the handle */
+    result = run_handle(dump_descriptor, dump_stream, handle, if_num);
+#else
+    (void)dump_descriptor;
+    (void)dump_stream;
+    (void)if_num;
+#endif
+
+cleanup:
+
+    /* Free the device */
+    if (handle != NULL)
+        libusb_close(handle);
+
+    /* Free device list along with devices */
+    if (list != NULL)
+        libusb_free_device_list(list, true);
+
+    /* Cleanup libusb context, if necessary */
+    if (ctx != NULL)
+        libusb_exit(ctx);
+
+    return result;
 }
 
 
@@ -61,7 +308,6 @@ main(int argc, char **argv)
          .name      = "help",
          .has_arg   = no_argument,
          .flag      = NULL},
-
         {.val       = 0,
          .name      = NULL,
          .has_arg   = 0,
@@ -70,13 +316,24 @@ main(int argc, char **argv)
 
     static const char  *short_opt_list = "h";
 
+    char        c;
+
     const char *entity;
     const char *bus_str;
     const char *dev_str;
-    const char *if_str;
+    const char *if_str      = "";
 
-    const char *arg_list[] = {&entity, &bus_str, &dev_str, &if_str, NULL};
-    size_t      i;
+    const char    **arg_list[] = {&entity, &bus_str, &dev_str, &if_str};
+    const size_t    req_arg_num = 3;
+    const size_t    max_arg_num = 4;
+    size_t          i;
+    char           *end;
+
+    bool            dump_descriptor = false;
+    bool            dump_stream     = false;
+    long            bus_num;
+    long            dev_num;
+    long            if_num          = -1;
 
     /*
      * Parse command line arguments
@@ -97,37 +354,56 @@ main(int argc, char **argv)
         }
     }
 
+#define USAGE_ERROR(_fmt, _args...) \
+    do {                                                \
+        fprintf(stderr, _fmt "\n", ##_args);            \
+        usage(stderr, program_invocation_short_name);   \
+        return 1;                                       \
+    } while (0)
+
     /*
      * Assign positional parameters
      */
-    for (i = 0;
-         i < sizeof(arg_list) / sizeof(*arg_list) && optind < argc;
-         i++, optind++)
-        arg_list = argv[optind];
+    for (i = 0; i < max_arg_num && optind < argc; i++, optind++)
+        *arg_list[i] = argv[optind];
 
-    if (*parg != NULL
+    if (i < req_arg_num)
+        USAGE_ERROR("Not enough arguments");
 
-    if (optind < argc)
+    /*
+     * Parse and verify positional parameters
+     */
+    if (strncmp(entity, "descriptor", strlen(entity)) == 0)
+        dump_descriptor = true;
+    else if (strncmp(entity, "stream", strlen(entity)) == 0)
+        dump_stream = true;
+    else if (strncmp(entity, "both", strlen(entity)) == 0)
     {
-        entity = argv[optind++];
-        if (optind < argc)
-        {
-            bus_str = argv[optind++];
-            if (optind < argc)
-            {
-                dev_str = argv[optind++];
-                if (optind < argc)
-                {
-                    if_str = argv[optind++];
-                    if (optind < argc)
-                    {
-                        fprintf(stderr, "Too many arguments\n");
-                        usage(stderr, program_invocation_short_name);
-                        return 1;
-                    }
-                }
-            }
-        }
+        dump_descriptor = true;
+        dump_stream = true;
+    }
+    else
+        USAGE_ERROR("Unknown entity \"%s\"", entity);
+
+    errno = 0;
+    bus_num = strtol(bus_str, &end, 0);
+    if (errno != 0 || !strisblank(end) || bus_num <= 0 || bus_num > 255)
+        USAGE_ERROR("Invalid bus number \"%s\"", bus_str);
+
+    errno = 0;
+    dev_num = strtol(dev_str, &end, 0);
+    if (errno != 0 || !strisblank(end) || dev_num <= 0 || dev_num > 255)
+        USAGE_ERROR("Invalid device number \"%s\"", dev_str);
+
+    if (!strisblank(if_str))
+    {
+        errno = 0;
+        if_num = strtol(if_str, &end, 0);
+        if (errno != 0 || !strisblank(end) || if_num < 0 || if_num >= 255)
+            USAGE_ERROR("Invalid interface number \"%s\"", if_str);
     }
 
+    return run(dump_descriptor, dump_stream, bus_num, dev_num, if_num);
 }
+
+
