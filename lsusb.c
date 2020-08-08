gregkh/usbutils

commit dddb696e01720eea60dfebba29000233e7585bdf

lsusb: Read unkown names from sysfs device desc.
lsusb tries to get the names for manufacturer and product of a device by
asking the udev hwdb for the names of the device id.
Not every manufacturer and device are in this database. Most devices
however provide those names in the standard USB descriptors, namely
iManufacturer and iProduct.
To get those with libusb we would have to open the device which
requires superuser privileges. To get those values anyway the kernel
provides them in sysfs.

Signed-off-by: Daniel Schaefer <git@danielschaefer.me>

lsusb.c
@@ -3693,7 +3693,7 @@ static int list_devices(libusb_context *ctx, int busnum, int devnum, int vendori
 	struct libusb_device_descriptor desc;
 	char vendor[128], product[128];
 	int status;
-	ssize_t num_devs, i;
+	ssize_t num_devs, i, vendor_len, product_len;
 
 	status = 1; /* 1 device not found, 0 device found */
 
@@ -3705,6 +3705,7 @@ static int list_devices(libusb_context *ctx, int busnum, int devnum, int vendori
 		libusb_device *dev = list[i];
 		uint8_t bnum = libusb_get_bus_number(dev);
 		uint8_t dnum = libusb_get_device_address(dev);
+		uint8_t pnum = libusb_get_port_number(dev);
 
 		if ((busnum != -1 && busnum != bnum) ||
 		    (devnum != -1 && devnum != dnum))
@@ -3714,9 +3715,18 @@ static int list_devices(libusb_context *ctx, int busnum, int devnum, int vendori
 		    (productid != -1 && productid != desc.idProduct))
 			continue;
 		status = 0;
-		get_vendor_string(vendor, sizeof(vendor), desc.idVendor);
-		get_product_string(product, sizeof(product),
+
+		vendor_len = get_vendor_string(vendor, sizeof(vendor), desc.idVendor);
+		if (vendor_len == 0)
+			read_sysfs_prop(vendor, sizeof(vendor), bnum, pnum,
+					"manufacturer");
+
+		product_len = get_product_string(product, sizeof(product),
 				desc.idVendor, desc.idProduct);
+		if (product_len == 0)
+			read_sysfs_prop(product, sizeof(product), bnum, pnum,
+					"product");
+
 		if (verblevel > 0)
 			printf("\n");
 		printf("Bus %03u Device %03u: ID %04x:%04x %s %s\n",

names.c
@@ -20,6 +20,7 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <ctype.h>
+#include <linux/limits.h>
 
 #include <libudev.h>
 
@@ -30,6 +31,8 @@
 #define HASH1  0x10
 #define HASH2  0x02
 #define HASHSZ 512
+
+#define SYSFS_DEV_ATTR_PATH "/sys/bus/usb/devices/%d-%d/%s"
 
 static unsigned int hashnum(unsigned int num)
 {
@@ -402,6 +405,27 @@ static void print_tables(void)
 	printf("--------------------------------------------\n");
 }
 */
+
+int read_sysfs_prop(char *buf, size_t size, uint8_t bnum, uint8_t pnum, char *propname)
+{
+	int n, fd;
+	char path[PATH_MAX];
+
+	buf[0] = '\0';
+	snprintf(path, sizeof(path), SYSFS_DEV_ATTR_PATH, bnum, pnum, propname);
+	fd = open(path, O_RDONLY);
+
+	if (fd == -1)
+		return 0;
+
+	n = read(fd, buf, size);
+
+	if (n > 0)
+		buf[n-1] = '\0';  // Turn newline into null terminator
+
+	close(fd);
+	return n;
+}
 
 int names_init(void)
 {

names.h
@@ -9,6 +9,7 @@
 #define _NAMES_H
 
 #include <sys/types.h>
+#include <stdint.h>
 
 /* ---------------------------------------------------------------------- */
 
@@ -33,6 +34,8 @@ extern int get_vendor_string(char *buf, size_t size, u_int16_t vid);
 extern int get_product_string(char *buf, size_t size, u_int16_t vid, u_int16_t pid);
 extern int get_class_string(char *buf, size_t size, u_int8_t cls);
 extern int get_subclass_string(char *buf, size_t size, u_int8_t cls, u_int8_t subcls);
+
+extern int read_sysfs_prop(char *buf, size_t size, uint8_t bnum, uint8_t pnum, char *propname);
 
 extern int names_init(void);
 extern void names_exit(void);

commit a3c0b414dbd7c46b87fc9ad3ea51643c9cd2b418

lsusb: Use vendor and product name fallback logic in -D mode too
commit dddb696e0172 ("lsusb: Read unkown names from sysfs device desc.")
introduced a fallback option for vendor and product names in device
summary lines, fetching them from sysfs when the VID/PID are not present
in udev hwdb. However, it applied this new logic only to summary lines
listing multiple devices (i.e. not -D mode, which only lists a single
device). Refactor the logic into a common function and use it in both
places.

Fixes: dddb696e0172 ("lsusb: Read unkown names from sysfs device desc.")
Signed-off-by: Thomas Hebb <tommyhebb@gmail.com>

lsusb.c
@@ -3663,6 +3663,38 @@ static void dumpdev(libusb_device *dev)
 
 /* ---------------------------------------------------------------------- */
 
+/*
+ * Attempt to get friendly vendor and product names from the udev hwdb. If
+ * either or both are not present, instead populate those from the device's
+ * own string descriptors.
+ */
+static void get_vendor_product_with_fallback(char *vendor, int vendor_len,
+					     char *product, int product_len,
+					     libusb_device *dev)
+{
+	struct libusb_device_descriptor desc;
+	char sysfs_name[PATH_MAX];
+	bool have_vendor, have_product;
+
+	libusb_get_device_descriptor(dev, &desc);
+
+	have_vendor = !!get_vendor_string(vendor, vendor_len, desc.idVendor);
+	have_product = !!get_product_string(product, product_len,
+			desc.idVendor, desc.idProduct);
+
+	if (have_vendor && have_product)
+		return;
+
+	if (get_sysfs_name(sysfs_name, sizeof(sysfs_name), dev) >= 0) {
+		if (!have_vendor)
+			read_sysfs_prop(vendor, vendor_len, sysfs_name,
+					"manufacturer");
+		if (!have_product)
+			read_sysfs_prop(product, product_len, sysfs_name,
+					"product");
+	}
+}
+
 static int dump_one_device(libusb_context *ctx, const char *path)
 {
 	libusb_device *dev;
@@ -3675,8 +3707,8 @@ static int dump_one_device(libusb_context *ctx, const char *path)
 		return 1;
 	}
 	libusb_get_device_descriptor(dev, &desc);
-	get_vendor_string(vendor, sizeof(vendor), desc.idVendor);
-	get_product_string(product, sizeof(product), desc.idVendor, desc.idProduct);
+	get_vendor_product_with_fallback(vendor, sizeof(vendor),
+			product, sizeof(product), dev);
 	printf("Device: ID %04x:%04x %s %s\n", desc.idVendor,
 					       desc.idProduct,
 					       vendor,
@@ -3691,7 +3723,7 @@ static int list_devices(libusb_context *ctx, int busnum, int devnum, int vendori
 	struct libusb_device_descriptor desc;
 	char vendor[128], product[128];
 	int status;
-	ssize_t num_devs, i, vendor_len, product_len;
+	ssize_t num_devs, i;
 
 	status = 1; /* 1 device not found, 0 device found */
 
@@ -3703,8 +3735,6 @@ static int list_devices(libusb_context *ctx, int busnum, int devnum, int vendori
 		libusb_device *dev = list[i];
 		uint8_t bnum = libusb_get_bus_number(dev);
 		uint8_t dnum = libusb_get_device_address(dev);
-		char sysfs_name[PATH_MAX];
-		get_sysfs_name(sysfs_name, sizeof(sysfs_name), dev);
 
 		if ((busnum != -1 && busnum != bnum) ||
 		    (devnum != -1 && devnum != dnum))
@@ -3715,16 +3745,8 @@ static int list_devices(libusb_context *ctx, int busnum, int devnum, int vendori
 			continue;
 		status = 0;
 
-		vendor_len = get_vendor_string(vendor, sizeof(vendor), desc.idVendor);
-		if (vendor_len == 0)
-			read_sysfs_prop(vendor, sizeof(vendor), sysfs_name,
-					"manufacturer");
-
-		product_len = get_product_string(product, sizeof(product),
-				desc.idVendor, desc.idProduct);
-		if (product_len == 0)
-			read_sysfs_prop(product, sizeof(product), sysfs_name,
-					"product");
+		get_vendor_product_with_fallback(vendor, sizeof(vendor),
+				product, sizeof(product), dev);
 
 		if (verblevel > 0)
 			printf("\n");